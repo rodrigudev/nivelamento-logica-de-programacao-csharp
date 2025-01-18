1e035e69d0abf098cf18f9828a0d87d7fbecf8bb
rodrigudev <rodrigu.dev@gmail.com>
2025-01-18 16:35:12 -0300

    SegundoExercicio

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14159, valorRaio, area;
+            Console.WriteLine("Digite o valor do Raio de um Círculo:");
+            valorRaio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = pi * (Math.Pow(valorRaio, 2));
+            Console.WriteLine("Àrea do Círculo: " + area.ToString("F4", CultureInfo.InvariantCulture));
+
+        }
+    }
+
+}
