54da14209a4bc4386b3db92644a7f42dec3e0bc5
Rodrigu-dev <rodrigo.five@gmail.com>
2025-01-18 15:25:32 -0300

    PrimeiroExercicio

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorUm, valorDois, valorDaSoma;
+
+            Console.WriteLine("Digite o Valor 1");
+            valorUm = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o Valor 2");
+            valorDois = int.Parse(Console.ReadLine());
+            valorDaSoma = valorUm + valorDois;
+            Console.WriteLine("Valor da Soma: " + valorDaSoma);
+        }
+    }
+
+}
+
