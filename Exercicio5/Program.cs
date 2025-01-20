70e860ba3f211016ce3a1d975d2611a39ab932c0
rodrigudev <rodrigu.dev@gmail.com>
2025-01-19 23:31:37 -0300

    QuintoExercicio

---
+++ Exercicio5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número para verificar:");
+            int numero = int.Parse(Console.ReadLine());
+            string mensagem = (numero >= 0) ? "Positivo" : "Negativo";
+            Console.WriteLine($"O número é: {mensagem}");
+
+        }
+    }
+}
