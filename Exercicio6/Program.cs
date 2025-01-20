d94eb11d3cac2743f232cf614fe0b8826eca8c84
rodrigudev <rodrigu.dev@gmail.com>
2025-01-20 20:54:31 -0300

    SextoExercicio

---
+++ Exercicio6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro");
+            int number = int.Parse(Console.ReadLine());
+            string result = (number % 2 == 0) ? "Par" : "Impar";
+
+            Console.WriteLine($"O Número é: {result}");
+        }
+    }
+}
