37f9b448211bf05d4ebab54eaf0e23f47e90b5d0
rodrigudev <rodrigu.dev@gmail.com>
2025-01-26 22:51:38 -0300

    DecimoPrimeiroExercicio

---
+++ Exercicio11/Program.cs
@@ -0,0 +1,27 @@
+﻿/* 
+Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha
+incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser
+impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002.
+*/
+using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a senha de 4 digitos numéricos");
+            int senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha invalida, digite novamente");
+                senha = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso Permitido");
+
+        }
+    }
+}
