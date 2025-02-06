33011cc0ae780a0088a4c1ef8ca0e1e68ede5ac3
rodrigudev <rodrigu.dev@gmail.com>
2025-02-05 22:28:30 -0300

    decimoQuintoExercicio

---
+++ Exercicio15/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um valor inteiro que representa a quantidade de valores a serem lidos: ");
+            int repeticao = int.Parse(Console.ReadLine());
+
+            int contDentroIntervalo = 0;
+
+            for (int i = 0; i < repeticao; i++)
+            {
+                Console.Write($"Digite o valor {i + 1}: ");
+                int valor = int.Parse(Console.ReadLine());
+
+                if (valor >= 10 && valor <= 20)
+                {
+                    contDentroIntervalo += 1;
+                }
+            }
+            Console.WriteLine($"Quantidade dentro do intervalor 10 e 20 é: {contDentroIntervalo}");
+            Console.WriteLine($"Quantidade fora do intervalo 10 e 20 é: {repeticao - contDentroIntervalo}");
+        }
+    }
+}
