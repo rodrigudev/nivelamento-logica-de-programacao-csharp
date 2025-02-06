ddcc25b2bad4a80388ecad040d5d6d960eeb9fce
rodrigudev <rodrigu.dev@gmail.com>
2025-02-06 20:23:28 -0300

    decimoSextoExercicio

---
+++ Exercicio16/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Digite um valor para encontrar seu fatorial");
+int valor = int.Parse(Console.ReadLine());
+int fatorial = 1;
+
+for (int i = 1; i <= valor; i++)
+{
+    fatorial = fatorial * i;
+}
+
+Console.WriteLine($"O fatorial de {valor} é: {fatorial}");
+
